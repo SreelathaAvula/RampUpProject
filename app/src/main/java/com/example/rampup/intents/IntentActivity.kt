544f5d634b7e88428fb4688e22d80a198980a991
SreelathaAvula <sreelatha@appscrip.co>
2023-06-26 17:35:12 +0530

    explicitIntent passing data and receiving in another Activity

--- app/src/main/java/com/example/rampup/intents/IntentActivity.kt
+++ app/src/main/java/com/example/rampup/intents/IntentActivity.kt
@@ -4,30 +4,49 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.example.rampup.R
 
 class IntentActivity : AppCompatActivity() {
-    lateinit var searchButton:Button
+    lateinit var searchButton: Button
     lateinit var editText: EditText
-    lateinit var expliciIntent:Button
+    lateinit var expliciIntent: Button
+
+    //explicit
+    lateinit var sendText: EditText
+    lateinit var sendButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent)
-        searchButton=findViewById(R.id.clickButton)
-        editText=findViewById(R.id.editText)
-        searchButton.setOnClickListener{
+        searchButton = findViewById(R.id.clickButton)
+        editText = findViewById(R.id.editText)
+        searchButton.setOnClickListener {
             startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(editText.text.toString())))
         }
-        expliciIntent=findViewById(R.id.explicitButton)
+        expliciIntent = findViewById(R.id.explicitButton)
         expliciIntent.setOnClickListener {
             reDirectToSecondActivity()
         }
 
-    }
 
+        //explicit
+
+        sendText = findViewById(R.id.explicitEditText)
+
+        sendButton = findViewById(R.id.sendButton)
+
+        sendButton.setOnClickListener {
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            var intent: Intent = Intent(this@IntentActivity, SecondActivity::class.java)
+            intent.putExtra("msg_key", sendText.text.toString())
+            startActivity(intent)
+        }
+    }
     private fun reDirectToSecondActivity() {
-startActivity(Intent(this,SecondActivity::class.java))    }
+        startActivity(Intent(this, SecondActivity::class.java))
+    }
 }
--- app/src/main/java/com/example/rampup/intents/SecondActivity.kt
+++ app/src/main/java/com/example/rampup/intents/SecondActivity.kt
@@ -1,12 +1,18 @@
 package com.example.rampup.intents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.rampup.R
 
 class SecondActivity : AppCompatActivity() {
+    lateinit var textReceive:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        textReceive=findViewById(R.id.receiverText)
+        textReceive.text=  intent.getStringExtra("msg_key")
+
     }
 }
